912e1a0904163041afcba08e335d9d7f7545443d
Jakob Kielland <jakob-ek@hotmail.com>
2022-12-06 07:53:00 +0100

    add day 6

---
+++ 06/06a.py
@@ -0,0 +1,20 @@
+f = open("06/input.txt", "r").read()
+n_uniques = 4
+
+buffer = [None] * n_uniques
+counter = 0
+
+for i in range(len(f)):
+  buffer.pop(0)
+  counter += 1
+
+  if f[i] in buffer:
+    reversed_buffer = buffer.copy()
+    reversed_buffer.reverse()
+    counter = min(reversed_buffer.index(f[i]) + 1, counter)
+
+  buffer.append(f[i])
+
+  if counter >= n_uniques:
+    print("found marker:", i + 1)
+    break
