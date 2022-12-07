a923ff38adb36f0837f8557423fa692e07023138
Jakob Kielland <jakob-ek@hotmail.com>
2022-12-07 08:49:08 +0100

    make day 7 a bit more concise by using defaultdict

--- 07/07b.py
+++ 07/07b.py
@@ -1,13 +1,9 @@
+from collections import defaultdict
+
 f = open("07/input.txt", "r").read().splitlines()
 
 path = ""
-directory = {}
-
-# adds size to path in dict, creates new dict entry if needed
-def add_to_path(path, size):
-  if path not in directory:
-      directory.update({path: 0})
-  directory.update({path: directory[path] + size})
+directory = defaultdict(int)
 
 def up(path):
   return path[:path.rfind("/")]
@@ -19,7 +15,7 @@ for c in f:
     if c[2] == "c":
       if c == "$ cd ..":
         # update directory above
-        add_to_path(up(path), directory[path])
+        directory[up(path)] += directory[path]
         path = up(path)
       elif c != "$ cd /":
         if path == "" or path[-1] != "/":
@@ -31,13 +27,13 @@ for c in f:
   # is file
   else:
     file_size = int(c.split(" ")[0])
-    add_to_path(path, file_size)
+    directory[path] += file_size
 
 # return to root path, update backwards along the way
 while path != "":
   temp_path = path
   while temp_path != "":
-    add_to_path(up(temp_path), directory[temp_path])
+    directory[up(temp_path)] += directory[temp_path]
     temp_path = up(temp_path)
   path = up(path)
 
