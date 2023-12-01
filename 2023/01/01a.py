575f2d16c1df9385150c491c23d172431971a279
Jakob Kielland <jakob-ek@hotmail.com>
2023-12-01 15:44:50 +0100

    undelete solution a

--- 2023/01/01a.py
+++ 2023/01/01a.py
@@ -5,37 +5,12 @@ with open("01/input.txt", "r") as f:
 
 sum = 0
 
-def convert_to_number(text):
-  if text.isnumeric():
-    return text
-  match text:
-    case "one":
-      return 1
-    case "two":
-      return 2
-    case "three":
-      return 3
-    case "four":
-      return 4
-    case "five":
-      return 5
-    case "six":
-      return 6
-    case "seven":
-      return 7
-    case "eight":
-      return 8
-    case "nine":
-      return 9
-
 def find_number_as_string(text, reversed):
-  raw_matches = re.finditer(r'(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))', text)
-  matches = [match.group(1) for match in raw_matches]
+  matches = re.findall("\d", text)
   if reversed:
     result = matches[-1]
   else:
     result = matches[0]
-  result = convert_to_number(result)
   return str(result)
 
 for line in splitfile:
